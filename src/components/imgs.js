460255239b65d1ef2ff689c61148e30a146dd032
AhmedAbderrahmanElBaghdadi04 <baghdadiahmed004@gmail.com>
2023-11-05 15:43:34 +0100

    first

---
+++ src/components/imgs.js
@@ -0,0 +1,8 @@
+import img1 from './wp/assilah.jpg';
+import img2 from './wp/gtavc.jpg';
+import img3 from './wp/lhafa.jpg';
+import img4 from './wp/sokdbara.jpg';
+import img5 from './wp/tangerPlaya.jpg';
+import img6 from './wp/tangier1.jpg';
+
+export default ImageData = [img1,img2,img3,img4,img5,img6];
