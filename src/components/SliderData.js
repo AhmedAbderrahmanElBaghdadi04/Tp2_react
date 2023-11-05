566ae4953708469382fb50902c60cb9a551733b7
AhmedAbderrahmanElBaghdadi04 <baghdadiahmed004@gmail.com>
2023-11-05 15:31:17 +0100

    first

---
+++ src/components/SliderData.js
@@ -0,0 +1,23 @@
+export const SliderData = [
+    {
+      image:
+      'https://tse2.mm.bing.net/th?id=OIP.X035hr4k4xjQla0QC9j3HAHaE-&pid=Api&P=0&h=180'
+    },
+    {
+      image:
+        'https://tse4.mm.bing.net/th?id=OIP.9dS7ubaH8lqf8PkaLTh3_gHaE8&pid=Api&P=0&h=180'
+    },
+    {
+      image:
+        'https://external-preview.redd.it/HnEUWSEsVFtRXyb3A0YJrLO0t1SyujKaDL0UseFp6Qc.jpg?auto=webp&s=5b6010c75b06ac5833ac9ebf96d2bb7be866bede'
+    },
+    {
+      image:
+        'https://content.r9cdn.net/rimg/dimg/a3/3d/fb553b4f-city-15337-16567bc0854.jpg?width=1750&height=1000&xhint=1861&yhint=1564&crop=true'
+    },
+    {
+      image:
+        'https://mediaim.expedia.com/localexpert/298372/34e94622-2af1-413f-8f19-a2296368d31b.jpg?impolicy=resizecrop&rw=1005&rh=565'
+    }
+  ];
+  
