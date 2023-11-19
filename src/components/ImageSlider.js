50dc1bfe87ef4f23ec3fdd07fbcf69fda0b62c9f
AhmedAbderrahmanElBaghdadi04 <baghdadiahmed004@gmail.com>
2023-11-19 18:49:30 +0100

    fst

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -1,39 +1,53 @@
 import React, { useState } from 'react';
+import { Container, Row, Col, Button, Image } from 'react-bootstrap';
 import { SliderData } from './SliderData';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../App.css';
 
 const ImageSlider = ({ slides }) => {
   const [current, setCurrent] = useState(0);
   const length = slides.length;
 
   const nextSlide = () => {
-    setCurrent(current === length - 1 ? 0 : current + 1);
+    setCurrent((current + 1) % length);
   };
 
   const prevSlide = () => {
-    setCurrent(current === 0 ? length - 1 : current - 1);
+    setCurrent((current - 1 + length) % length);
   };
 
-  if (!Array.isArray(slides) || slides.length <= 0) {
+  if (!Array.isArray(slides) || slides.length === 0) {
     return null;
   }
 
   return (
-    <section className='slider'>
-      <input type='button' value='<' className='left-arrow' onClick={prevSlide} />
-      <input type='button' value='>' className='right-arrow' onClick={nextSlide} />
-      {SliderData.map((slide, index) => {
-        return (
-          <div
-            className={index === current ? 'slide active' : 'slide'}
-            key={index}
-          >
-            {index === current && (
-              <img src={slide.image} alt='travel image' className='image' />
-            )}
-          </div>
-        );
-      })}
-    </section>
+    <Container fluid>
+      <Row className="justify-content-center align-items-center vh-100">
+        <Col md={1} className="text-center">
+          <Button variant='secondary' onClick={prevSlide}>&lt;</Button>
+        </Col>
+        <Col xs={12} md={10} className="text-center">
+          {SliderData.map((slide, index) => (
+            <div
+              className={index === current ? 'slide active' : 'slide'}
+              key={index}
+            >
+              {index === current && (
+                <Image
+                  src={slide.image}
+                  alt={`slide ${index}`}
+                  fluid
+                  className="center-image"
+                />
+              )}
+            </div>
+          ))}
+        </Col>
+        <Col md={1} className="text-center">
+          <Button variant='secondary' onClick={nextSlide}>&gt;</Button>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
